e76dff73fdfc90a3458d0baaeb3e3ae2a951c205
andresilveira <andresilveirah@gmail.com>
2015-08-11 15:32:52 +0200

    add home controller

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,18 @@
+class HomeController < ApplicationController
+  before_action :random_coords
+  
+  # TODO:
+  #  * move the initialization of the API to a initializer
+  #  * move the api_key to a config yaml
+  def index
+    @weather = OpenWeatherMap::API::JSON.new(api_key: "").weather_by_coordinates(@coords.lat, @coords.lon)
+  end
+  
+  private
+  
+  def random_coords
+    # TODO: refactor this concept of coordinates to it's own class
+    @coords = Coordinate.new(lat: rand(-90..90), lon: rand(-180..180))
+  end
+  
+end
--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,6 @@ module OpenWeatherMapApp
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
 
-    config.autoload_paths << Rails.root.join('app/services')
+    config.autoload_paths << Rails.root.join('app/services', 'app/data_types')
   end
 end

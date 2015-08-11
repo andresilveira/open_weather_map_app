9048aeee98ad35a2f320ae97ea8194c95036c493
andresilveira <andresilveirah@gmail.com>
2015-08-11 15:04:50 +0200

    add json to open_weather_map service

--- app/services/open_weather_map.rb
+++ app/services/open_weather_map.rb
@@ -11,5 +11,41 @@ class OpenWeatherMap
     def initialize(api_key:)
       @api_key = api_key
     end
+    
+    def weather_by_coordinates(lat, lon)
+      response = Net::HTTP.get_response(uri_for_weather_by_coordinates(lat.to_s, lon.to_s))
+      response.body if response.is_a?(Net::HTTPSuccess)
+    end
+    
+    def uri_for_weather_by_coordinates(lat, lon)
+      uri = API_URI
+      uri.query = URI.encode_www_form({lat: lat.to_s, lon: lon.to_s}.merge(DEFAULT_PARAMETERS))
+      uri
+    end
+    
+    class API::JSON < API
+      require 'json'
+
+      def weather_by_coordinates(lat, lon)
+        resp = ::JSON.parse(super(lat,lon))
+        
+        weather = resp["weather"].first
+        main = resp["main"]
+        sys = resp["sys"]
+
+        {
+          temperature:  main["temp"],           # depends on units
+          pressure:     main["pressure"],       # %
+          humidity:     main["humidity"],       # hPa
+          description:  weather["description"], 
+          icon:         weather["icon"],
+          wind_speed:   resp["wind"]["speed"],  # m/s
+          sunrise:      sys["sunrise"],         # time at UTC
+          sunset:       sys["sunset"],          # time at UTC
+          country:      sys["country"],         # country Code
+          city:         resp["name"]            # city name
+        }
+      end
+    end
   end
 end
--- test/services/open_weather_map_test.rb
+++ test/services/open_weather_map_test.rb
@@ -2,7 +2,29 @@ require 'test_helper'
 require 'minitest/autorun'
 
 describe OpenWeatherMap::API do
-  it 'it must exist' do
-    OpenWeatherMap::API.new(api_key: 'some_key').wont_be_nil
+  before do 
+   @wrapper = OpenWeatherMap::API.new(api_key: "")
+  end
+
+  it 'have an uri for weather by coordinates' do
+    @wrapper.uri_for_weather_by_coordinates(123,321).to_s.must_equal "http://api.openweathermap.org/data/2.5/weather?lat=123&lon=321&units=metric"
+  end
+  
+  describe JSON do
+    before do
+     @wrapper = OpenWeatherMap::API::JSON.new(api_key: "")
+    end
+
+    describe '#weather_by_coordinates' do
+      before(:all) do
+        @weather = @wrapper.weather_by_coordinates(1,1)
+      end
+      
+      [:temperature, :description, :icon, :wind_speed, :pressure, :humidity, :sunrise, :sunset, :city, :country].each do |attribute|
+        it "should contain #{attribute}" do
+          @weather.keys.include?(attribute).must_equal true
+        end
+      end
+    end
   end
 end

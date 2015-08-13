be018f194fbd33b2499caaba4d2bdcea302e7817
andresilveira <andresilveirah@gmail.com>
2015-08-13 17:46:16 +0200

    fix application.rb

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,5 @@ module OpenWeatherMapApp
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-
-    config.autoload_paths << Rails.root.join('app/services', 'app/data_types')
   end
 end
